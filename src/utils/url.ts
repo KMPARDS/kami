48ca5d3af6ebf4e52940886bf90aa85b79496915
zemse <sohamzemse143@gmail.com>
2020-05-24 22:14:33 +0530

    update url equality to prevent local duplicate nodes and test cases

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -13,10 +13,22 @@ export class URLMask {
   }
 
   eq(otherUrl: URLMask | string): boolean {
-    if (otherUrl instanceof URLMask) {
-      return this.toString() === otherUrl.toString();
-    } else {
-      return this.toString() === new URLMask(otherUrl).toString();
+    return this._eq(
+      otherUrl instanceof URLMask ? otherUrl : new URLMask(otherUrl)
+    );
+  }
+
+  // TODO: check if it's possible for same peer to accept requests from two IPs
+  //  If yes then some procedure needs to be followed, while checking equality
+  private _eq(otherUrl: URLMask): boolean {
+    const _thisUrl: URL = new URL(this.toString());
+    const _otherUrl: URL = new URL(otherUrl.toString());
+    if (_thisUrl.hostname === 'localhost') {
+      _thisUrl.hostname = '127.0.0.1';
     }
+    if (_otherUrl.hostname === 'localhost') {
+      _otherUrl.hostname = '127.0.0.1';
+    }
+    return _thisUrl.toString() === _otherUrl.toString();
   }
 }
--- test/utils/url.test.js
+++ test/utils/url.test.js
@@ -10,25 +10,41 @@ const testCases = [
 ];
 
 describe('URL', () => {
-  testCases.forEach((testCaseEntry) => {
-    it(`initiating URL with ${testCaseEntry[0]} ${
-      testCaseEntry[1] ? 'should work' : 'should give error'
-    }`, () => {
-      let isUrlInitiated = false;
-      try {
-        const url = new URLMask(testCaseEntry[0]);
-        isUrlInitiated = true;
-      } catch (error) {
-        // console.log(error);
-      }
+  describe('Constructor', () => {
+    testCases.forEach((testCaseEntry) => {
+      it(`initiating URL with ${testCaseEntry[0]} ${
+        testCaseEntry[1] ? 'should work' : 'should give error'
+      }`, () => {
+        let isUrlInitiated = false;
+        try {
+          const url = new URLMask(testCaseEntry[0]);
+          isUrlInitiated = true;
+        } catch (error) {
+          // console.log(error);
+        }
 
-      assert.strictEqual(
-        isUrlInitiated,
-        testCaseEntry[1],
-        testCaseEntry[1]
-          ? 'should not give error but it did'
-          : "should give error but it didn't"
-      );
+        assert.strictEqual(
+          isUrlInitiated,
+          testCaseEntry[1],
+          testCaseEntry[1]
+            ? 'should not give error but it did'
+            : "should give error but it didn't"
+        );
+      });
+    });
+  });
+
+  describe('Equality', () => {
+    it('local urls with same port are equal', () => {
+      const local1 = new URLMask('http://localhost:25985');
+      const local2 = new URLMask('http://127.0.0.1:25985');
+      assert.ok(local1.eq(local2), 'should be same');
+    });
+
+    it('local urls with diff port are not equal', () => {
+      const local1 = new URLMask('http://localhost:25985');
+      const local2 = new URLMask('http://127.0.0.1:5197');
+      assert.ok(!local1.eq(local2), 'should be diff');
     });
   });
 });

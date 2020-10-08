013d47c2c6d8c90559a2c625f0e8b4816b35394e
zemse <sohamzemse143@gmail.com>
2020-10-08 06:34:40 +0530

    Add recursive logic while computing bunch (#69)

--- src/informer/to-eth/methods/compute-bunch-proposal.ts
+++ src/informer/to-eth/methods/compute-bunch-proposal.ts
@@ -3,6 +3,7 @@ import { fetchBlocks } from '../../../utils/provider';
 import { t, validateParam } from '../../../type-validation';
 import { Bytes32 } from '../../../utils/bytes';
 import { BunchProposal } from '../../../utils/bunch-proposal';
+import { hexlify, keccak256, concat } from 'ethers/lib/utils';
 
 export async function computeBunchProposal(
   startBlockNumber: number,
@@ -10,6 +11,31 @@ export async function computeBunchProposal(
 ): Promise<BunchProposal> {
   validateParam({ startBlockNumber }, t.uint);
   validateParam({ bunchDepth }, t.uint);
+
+  if (bunchDepth >= 5) {
+    const left = await computeBunchProposal(startBlockNumber, bunchDepth - 1);
+    const right = await computeBunchProposal(
+      startBlockNumber + 2 ** (bunchDepth - 1) - 1,
+      bunchDepth - 1
+    );
+    const lastBlock = await global.providerEsn.getBlock(
+      startBlockNumber + 2 ** bunchDepth - 1
+    );
+    return {
+      startBlockNumber,
+      bunchDepth,
+      transactionsMegaRoot: hexlify(
+        keccak256(
+          concat([left.transactionsMegaRoot, right.transactionsMegaRoot])
+        )
+      ),
+      receiptsMegaRoot: hexlify(
+        keccak256(concat([left.receiptsMegaRoot, right.receiptsMegaRoot]))
+      ),
+      lastBlockHash: lastBlock.hash,
+      signatures: [],
+    };
+  }
 
   const blocks = await fetchBlocks(
     startBlockNumber,
